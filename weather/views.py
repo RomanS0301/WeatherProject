07df69f636ae0935e9155f338e3c0ed1b9bda0de
roman <roman.sidoruk@gmail.com>
2024-01-13 21:43:08 +0300

    Вывод информации о температкре нужного города

--- weather/views.py
+++ weather/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+import requests
 
 
 def index(request):
+    appid = '90d9088ba4add0f831fd2f3904a20649'
+    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + appid
+    city = 'Brest'
+    res = requests.get(url.format(city))
+    print(res.text)
     return render(request, 'weather/index.html')
 
 
