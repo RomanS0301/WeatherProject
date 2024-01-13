2a86299b4a0c3fb669d8a1f2f8c11e6a613b8f9b
roman <roman.sidoruk@gmail.com>
2024-01-13 20:37:31 +0300

    Создание главной страницы сайта

--- weather/views.py
+++ weather/views.py
@@ -1,16 +1,8 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth import login
-from django.contrib.auth.models import User
-from django.db import IntegrityError
+from django.shortcuts import render
 
 
 def index(request):
     return render(request, 'weather/index.html')
 
 
-def signupuser(request):
-    """
-    Создание нового пользователя, проверка на совпадение паролей, проверка на уникальность пользователя, запрет на
-    посещение пользователем админ панели, перенаправление пользователя на страницу с текущими задачами
-    """
-    return render(request, 'weather/signupuser.html')
+
